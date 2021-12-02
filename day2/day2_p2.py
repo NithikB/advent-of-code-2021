8336b0f4d8dc1c6228cbb2dc3d3a9ce0f47391a6
Nithik Balachandran <nithikbala@gmail.com>
2021-12-02 00:47:11 -0500

    day 2

---
+++ day2/day2_p2.py
@@ -0,0 +1,19 @@
+# Day 2: Dive! - Part 2
+
+# import input file into an array
+with open("day2_input.txt", 'r') as f:
+    commands = f.read().splitlines()
+
+pos, depth, aim = 0, 0, 0
+for command in commands:
+    dir, val = command.split(" ")
+    val = int(val)
+    if dir == "up":
+        aim -= val
+    elif dir == "down":
+        aim += val
+    else:
+        pos += val
+        depth += aim * val
+
+print(pos*depth)
