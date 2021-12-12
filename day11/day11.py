711f8792a58e92bad7e9aa18c777aad6cf09c5a6
Nithik Balachandran <nithikbala@gmail.com>
2021-12-12 17:49:17 -0500

    up to day 12

--- day11/day11.py
+++ day11/day11.py
@@ -2,14 +2,63 @@
 # https://adventofcode.com/2021/day/11
 
 
-# import input file into array of lines
+# import input file into grid of octopi
 with open("day11_input.txt", 'r') as f:
-    lines = f.read().splitlines()
+    grid = f.read().splitlines()
+    for i in range(len(grid)):
+        grid[i] = list(map(int, list(grid[i])))
 
 
-# print("Score:", score)
+def flash(r, c):
+    if r < 0 or r >= 10 or c < 0 or c >= 10 or (r, c) in flashed:
+        return 0
+
+    grid[r][c] += 1
+    dir = [(0, 1), (1, 0), (-1, 0), (0, -1),
+           (1, 1), (-1, -1), (1, -1), (-1, 1)]
+    if grid[r][c] > 9:
+        grid[r][c] = 0
+        flashed.add((r, c))
+        count = 1
+        for x, y in dir:
+            count += flash(r + x, c + y)
+        return count
+
+    return 0
+
+
+flashes = 0
+for i in range(100):
+    flashed = set()
+    for r in range(10):
+        for c in range(10):
+            grid[r][c] += 1
+    for r in range(10):
+        for c in range(10):
+            if grid[r][c] > 9:
+                flashes += flash(r, c)
+
+print("Flashes:", flashes)
 
 # Day 11 - Dumbo Octopus - Part 2
+with open("day11_input.txt", 'r') as f:
+    grid = f.read().splitlines()
+    for i in range(len(grid)):
+        grid[i] = list(map(int, list(grid[i])))
+        
+flashes = 0
+i = 0
+while True:
+    i += 1
+    flashed = set()
+    for r in range(10):
+        for c in range(10):
+            grid[r][c] += 1
+    for r in range(10):
+        for c in range(10):
+            if grid[r][c] > 9:
+                flashes += flash(r, c)
+    if len(flashed) == 100:
+        break
 
-
-# print("Middle score:", scores[len(scores) // 2])
+print("Step All Flashed:", i)
---
+++ day13/day13.py
@@ -0,0 +1,15 @@
+# Day 13 - P - Part 1
+# https://adventofcode.com/2021/day/13
+
+
+# import input file into array of lines
+with open("day12_input.txt", 'r') as f:
+    lines = f.read().splitlines()
+
+
+# print("Distinct Paths:", count)
+
+# Day 13 - P - Part 2
+
+
+# print("Distinct Paths:", count)
